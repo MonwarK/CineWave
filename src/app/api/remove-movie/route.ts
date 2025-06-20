9395e204bdbea523aedd87b912a682b63670a682
Michael Connor <necrydark@gmail.com>
2025-06-20 14:10:59 +0100

    added watchlist and started on removal of list items

---
+++ src/app/api/remove-movie/route.ts
@@ -0,0 +1,27 @@
+import { supabase } from "@/libs/supabaseClient";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const { userId } = getAuth(req);
+
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const body = await req.json();
+  const { movie_id } = body;
+
+
+  const { data, error } = await supabase
+    .from("saved_movies")
+    .delete()
+    .match({ movie_id: movie_id})
+    .select();
+
+  if (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+
+  return NextResponse.json({ data }, { status: 200 });
+}
---
+++ src/components/watchlist/WatchlistSection.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { SavedMovie } from "@/types/SavedMovies";
+import { useState } from "react";
+import MovieRowItem from "../discover/MovieRowItem";
+
+interface Props {
+  savedMovies: SavedMovie[]
+}
+
+export default function WatchlistSection({ savedMovies }: Props) {
+  const [selectSaved, setSelectedSave] = useState(null);
+
+  console.log(selectSaved);
+  return (
+    <>
+      <div className="py-10 space-y-18">
+        <div className="flex justify-center flex-wrap gap-4">
+          {savedMovies.map((saved: SavedMovie) => (
+            <MovieRowItem
+              savedMovie={saved}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
