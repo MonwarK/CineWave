0f9c4521f279c4d018ada4643aa121fec3cd4a39
Monwar Khan <monwar259@gmail.com>
2025-06-21 01:09:24 +0100

    Implemented header back into my list page

--- src/app/(protected)/list/page.tsx
+++ src/app/(protected)/list/page.tsx
@@ -1,16 +1,20 @@
 "use client";
+import Header from "@/components/main/Header";
 import WatchlistSection from "@/components/watchlist/WatchlistSection";
 import { useSavedMovies } from "@/context/SavedMoviesProvider";
 
 export default function ListPage() {
-  const { savedMovies, isSaved } = useSavedMovies();
+  const { savedMovies } = useSavedMovies();
 
   return (
-    <div className="pt-[7rem] container max-w-6xl mx-auto flex flex-col">
-      <h1 className="leading-2 text-3xl font-semibold text-center">
-        Your Watchlist
-      </h1>
-      <WatchlistSection savedMovies={savedMovies} />
+    <div>
+      <Header />
+      <div className="pt-[7rem] container max-w-6xl mx-auto flex flex-col">
+        <h1 className="leading-2 text-3xl font-semibold text-center">
+          Your Watchlist
+        </h1>
+        <WatchlistSection savedMovies={savedMovies} />
+      </div>
     </div>
   );
 }
