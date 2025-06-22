bfa8d6fdec6bf5288b324d7aa4428c614357e94d
Michael Connor <necrydark@gmail.com>
2025-06-22 16:53:06 +0100

    added

--- src/app/(protected)/trending/page.tsx
+++ src/app/(protected)/trending/page.tsx
@@ -14,7 +14,7 @@ export default async function TrendingPage() {
 
         <div className="pt-20 max-w-screen-xl mx-auto w-full px-7">
             <h1 className="text-3xl font-semibold">Trending</h1>
-            <p className="text-gray-300 text-sm">View all of the recently trending movies & tv shows.</p>
+            <p className="text-gray-300 text-sm mt-2">View all of the recently trending movies & tv shows.</p>
             <Tabs shows={popularTv} movies={popularMovies} />
         </div>
     </div>
