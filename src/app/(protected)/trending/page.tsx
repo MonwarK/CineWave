d1868da81d2dd719e2bbdff4a4115a35df76ee4e
Michael Connor <necrydark@gmail.com>
2025-06-22 15:10:50 +0100

    trending page with animations

---
+++ src/app/(protected)/trending/page.tsx
@@ -0,0 +1,22 @@
+import Header from "@/components/main/Header";
+import Tabs from "@/components/trending/Tabs";
+import { fetchPopularMovies, fetchPopularTV } from "@/utils/api";
+
+export default async function TrendingPage() {
+    const popularTv = await fetchPopularTV();
+    const popularMovies = await fetchPopularMovies(); 
+
+    console.log("TV Shows",popularTv);
+    console.log("Movies", popularMovies)
+  return (
+    <div className="py-10">
+        <Header /> 
+
+        <div className="pt-20 max-w-screen-xl mx-auto w-full px-7">
+            <h1 className="text-3xl font-semibold">Trending</h1>
+            <p className="text-gray-300 text-sm">View all of the recently trending movies & tv shows.</p>
+            <Tabs shows={popularTv} movies={popularMovies} />
+        </div>
+    </div>
+  )
+}
---
+++ src/components/trending/Tabs.tsx
@@ -0,0 +1,75 @@
+'use client'
+import { Movie } from '@/types/Movie';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
+import MovieLandscapeThumbnail from '../movie-card/MovieLandscapeThumbnail';
+
+interface Props {
+    shows: Movie[]
+    movies: Movie[]
+}
+
+export default function Tabs({shows, movies}: Props) {
+    const [isActive, setIsActive] = useState(0);
+
+
+  return (
+    <div>
+        <div className='flex flex-row justify-center space-x-4  rounded-md w-fit p-2 mx-auto items-center mb-4'>
+            <button className={ "px-4 py-2 rounded-md transition duration-300 " + (isActive === 0 ? "bg-orange-900" : "bg-black/50")} onClick={() => setIsActive(0)} >Movies</button>
+            <button className={"px-4 py-2 rounded-md transition duration-300 " + (isActive === 1 ? "bg-orange-900" : "bg-black/50")} onClick={() => setIsActive(1)}>Shows</button>
+        </div>
+        <div>
+        {isActive === 0 ? (
+                <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                   <AnimatePresence>
+                   {movies.map((movie: Movie) => (
+                      <motion.div
+                      key={movie.id}
+                      initial={{ y: 50, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      exit={{ y: 50, opacity: 0 }}
+                      transition={{
+                        type: 'spring',
+                        duration: 1,
+                        delay: 0.25,
+                        stiffness: 260,
+                        damping: 20
+                      }}
+                      >
+                          <MovieLandscapeThumbnail 
+                        mediaType='movie'
+                        key={`movie-thumbnail-${movie.id}`}
+                        movie={movie} />
+                      </motion.div>
+                    ))}
+                   </AnimatePresence>
+                </div>
+            ) : (
+                <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {shows.map((show: Movie) => (
+                         <motion.div
+                         key={show.id}
+                         initial={{ y: 50, opacity: 0 }}
+                         animate={{ y: 0, opacity: 1 }}
+                         exit={{ y: 50, opacity: 0 }}
+                         transition={{
+                           type: 'spring',
+                           duration: 1,
+                           delay: 0.25,
+                           stiffness: 260,
+                           damping: 20
+                         }}
+                         >
+                    <MovieLandscapeThumbnail 
+                    mediaType='tv'
+                    key={`movie-thumbnail-${show.id}`}
+                    movie={show} />
+                    </motion.div>
+                ))}
+            </div>
+            )}
+        </div>
+    </div>
+  )
+}
