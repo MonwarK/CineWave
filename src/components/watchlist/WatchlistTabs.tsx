f2e0b087abe45b0c140a374f9e8f75ba5ea3d083
Monwar Khan <monwar259@gmail.com>
2025-06-22 18:39:12 +0100

    Adjusted watchlist style

--- src/components/watchlist/WatchlistTabs.tsx
+++ src/components/watchlist/WatchlistTabs.tsx
@@ -10,67 +10,45 @@ interface Props {
 }
 
 export default function WatchlistTabs({ SavedMovies }: Props) {
-  const movies = SavedMovies.filter(x => x.isMovie === true)
-  const shows = SavedMovies.filter(x => x.isMovie === false)
-  
+  const movies = SavedMovies.filter(x => x.isMovie === true);
+  const shows = SavedMovies.filter(x => x.isMovie === false);
+
   const [isActive, setIsActive] = useState(movies.length > 0 ? 0 : 1);
 
   return (
     <div>
-      <div className="flex flex-row justify-center space-x-4  rounded-md w-fit p-2 mx-auto items-center mb-4">
+      <div className="flex justify-center space-x-4 rounded-md w-fit p-2 mx-auto items-center mb-4">
         {shows.length > 0 && movies.length > 0 && (
           <React.Fragment>
-          <button
-          className={clsx(
-            isActive === 0 ? 'bg-orange-900' : 'bg-black/50',
-            'px-4 py-2 rounded-md transition duration-300 cursor-pointer'
-          )}
-          onClick={() => setIsActive(0)}
-        >
-          Movies
-        </button>
-        <button
-          className={clsx(
-            isActive === 1 ? 'bg-orange-900' : 'bg-black/50',
-            'px-4 py-2 rounded-md transition duration-300 cursor-pointer'
-          )}
-          onClick={() => setIsActive(1)}
-        >
-          Shows
-        </button></React.Fragment>
+            <button
+              className={clsx(
+                isActive === 0 ? 'bg-orange-900' : 'bg-black/50',
+                'px-4 py-2 rounded-md transition duration-300 cursor-pointer'
+              )}
+              onClick={() => setIsActive(0)}
+            >
+              Movies
+            </button>
+            <button
+              className={clsx(
+                isActive === 1 ? 'bg-orange-900' : 'bg-black/50',
+                'px-4 py-2 rounded-md transition duration-300 cursor-pointer'
+              )}
+              onClick={() => setIsActive(1)}
+            >
+              Shows
+            </button>
+          </React.Fragment>
         )}
       </div>
+
       <div>
         {isActive === 0 ? (
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="flex gap-5 flex-wrap">
             <AnimatePresence>
-              {movies.map(
-                (movie: SavedMovie) => (
-                  <motion.div
-                    key={movie.id}
-                    initial={{ y: 50, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    exit={{ y: 50, opacity: 0 }}
-                    transition={{
-                      type: 'spring',
-                      duration: 1,
-                      delay: 0.25,
-                      stiffness: 260,
-                      damping: 20,
-                    }}
-                  >
-                    <MovieRowItem key={movie.id} savedMovie={movie} />
-                  </motion.div>
-                )
-              )}
-            </AnimatePresence>
-          </div>
-        ) : (
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {shows.map(
-              (show: SavedMovie) => (
+              {movies.map((movie: SavedMovie) => (
                 <motion.div
-                  key={show.id}
+                  key={movie.id}
                   initial={{ y: 50, opacity: 0 }}
                   animate={{ y: 0, opacity: 1 }}
                   exit={{ y: 50, opacity: 0 }}
@@ -82,10 +60,30 @@ export default function WatchlistTabs({ SavedMovies }: Props) {
                     damping: 20,
                   }}
                 >
-                  <MovieRowItem key={show.id} savedMovie={show} />
+                  <MovieRowItem key={movie.id} savedMovie={movie} />
                 </motion.div>
-              )
-            )}
+              ))}
+            </AnimatePresence>
+          </div>
+        ) : (
+          <div className="flex flex-wrap gap-5">
+            {shows.map((show: SavedMovie) => (
+              <motion.div
+                key={show.id}
+                initial={{ y: 50, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: 50, opacity: 0 }}
+                transition={{
+                  type: 'spring',
+                  duration: 1,
+                  delay: 0.25,
+                  stiffness: 260,
+                  damping: 20,
+                }}
+              >
+                <MovieRowItem key={show.id} savedMovie={show} />
+              </motion.div>
+            ))}
           </div>
         )}
       </div>
