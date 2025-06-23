de24934b4337784eeda1c1de49a0e8561f8d4576
Monwar Khan <monwar259@gmail.com>
2025-06-23 15:58:05 +0100

    slight styling changes to trailer

--- src/components/movie-content/MovieBanner.tsx
+++ src/components/movie-content/MovieBanner.tsx
@@ -3,9 +3,10 @@
 import SquaredButton from '@/components/ui/SquaredButton';
 import { useSavedMovies } from '@/context/SavedMoviesProvider';
 import { Movie } from '@/types/Movie';
-import { Check, Heart, Play, Plus, Share2 } from 'lucide-react';
+import { Check, Clapperboard, Heart, Play, Plus, Share2 } from 'lucide-react';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
+import { useExtractColors } from 'react-extract-colors';
 
 export default function MovieBanner({
   movie,
@@ -16,40 +17,48 @@ export default function MovieBanner({
 }) {
   const { addMovie, isSaved } = useSavedMovies();
   const isMovie = movie.title ? true : false;
-  const [trailerKey, setTrailerKey] = useState("")
+  const [trailerKey, setTrailerKey] = useState('');
+
+  const proxiedImage = `/api/image-proxy?url=${encodeURIComponent(
+    `https://image.tmdb.org/t/p/w1920${movie.backdrop_path}`
+  )}`;
+
+  const color = useExtractColors(proxiedImage);
+
+  console.log(color);
 
   useEffect(() => {
     document.title = `${movie.title || movie.name} | CineWave`;
   }, [movie]);
 
   useEffect(() => {
-    if(!movie) return;
-    
-    const mediaType = movie.media_type || (movie.name ? "tv": "movie")    
+    if (!movie) return;
+
+    const mediaType = movie.media_type || (movie.name ? 'tv' : 'movie');
 
     async function fetchTrailer() {
-    if(!movie) return;
-     
+      if (!movie) return;
+
       try {
-        const res = await  fetch(
+        const res = await fetch(
           `https://api.themoviedb.org/3/${mediaType}/${movie.id}/videos?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
         );
         const data = await res.json();
         const trailer = data.results?.find(
           (v: any) =>
-            (v.type === "Trailer" ||
-              v.type === "Featurette" ||
-              v.type === "Clip" ||
-              v.type === "Opening Credits") &&
-            v.site === "YouTube"
+            (v.type === 'Trailer' ||
+              v.type === 'Featurette' ||
+              v.type === 'Clip' ||
+              v.type === 'Opening Credits') &&
+            v.site === 'YouTube'
         );
-        setTrailerKey(trailer?.key || "")
+        setTrailerKey(trailer?.key || '');
       } catch (err) {
-        console.error("Failed to fetch movie details or trailer", err)
-      } 
-    } 
+        console.error('Failed to fetch movie details or trailer', err);
+      }
+    }
     fetchTrailer();
-  }, [movie])
+  }, [movie]);
 
   return (
     <div className="flex flex-col justify-center items-center p-0">
@@ -61,70 +70,86 @@ export default function MovieBanner({
           />
         </div>
 
-        <div className="relative z-0 p-4 mx-auto w-full container space-y-10 mb-5 mt-20 flex items-center space-x-4 justify-between">
-         <div className='space-y-5 lg:w-1/2' >
-         <div className="space-y-5">
-            <h1 className="text-4xl lg:text-5xl font-bold mb-5">
-              {movie.title || movie.name}
-            </h1>
-            {movie.tagline && (
-              <p className="text-xl text-gray-300 italic">"{movie.tagline}"</p>
-            )}
-            <p className="text-sm leading-relaxed">{movie.overview}</p>
-          </div>
+        <div className="relative z-0 p-4 mx-auto w-full container space-y-10 mb-5 mt-20 flex items-center space-x-4 justify-between gap-15">
+          <div className="space-y-5 flex-1">
+            <div className="space-y-5">
+              <h1 className="text-4xl lg:text-5xl font-bold mb-5">
+                {movie.title || movie.name}
+              </h1>
+              {movie.tagline && (
+                <p className="text-xl text-gray-300 italic">
+                  "{movie.tagline}"
+                </p>
+              )}
+              <p className="text-sm leading-relaxed">{movie.overview}</p>
+            </div>
 
-          <div className="flex flex-wrap gap-3">
-            <Link href={link}>
-              <SquaredButton>
-                <Play className="w-5 h-5" />
-                <p>Watch Now {!isMovie && 'S1 E1'}</p>
-              </SquaredButton>
-            </Link>
-
-            {isSaved(movie.id) ? (
-              <React.Fragment>
-                <SquaredButton variant="secondary">
-                  <Check className="w-5 h-5" />
-                  <p>Added</p>
+            <div className="flex flex-wrap gap-3">
+              <Link href={link}>
+                <SquaredButton>
+                  <Play className="w-5 h-5" />
+                  <p>Watch Now {!isMovie && 'S1 E1'}</p>
                 </SquaredButton>
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                <SquaredButton
-                  onClick={() => addMovie(movie)}
-                  variant="secondary"
+              </Link>
+
+              {trailerKey && (
+                <Link
+                  className="xl:hidden"
+                  href={`https://www.youtube.com/watch?v=${trailerKey}`}
+                  target="_blank"
                 >
-                  <Plus className="w-5 h-5" />
-                  <p>Watchlist</p>
-                </SquaredButton>
-              </React.Fragment>
-            )}
+                  <SquaredButton variant="secondary">
+                    <Clapperboard className="w-5 h-5" />
+                    <p>Play Trailer</p>
+                  </SquaredButton>
+                </Link>
+              )}
 
-            <SquaredButton variant="secondary">
-              <Heart className="w-5 h-5" />
-              <p>Favorite</p>
-            </SquaredButton>
-            <SquaredButton
-              onClick={() => {
-                navigator.clipboard.writeText(window.location.href);
-              }}
-              variant="secondary"
-            >
-              <Share2 className="w-5 h-5" />
-              <p>Share</p>
-            </SquaredButton>
+              {isSaved(movie.id) ? (
+                <React.Fragment>
+                  <SquaredButton variant="secondary">
+                    <Check className="w-5 h-5" />
+                    <p>Added</p>
+                  </SquaredButton>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <SquaredButton
+                    onClick={() => addMovie(movie)}
+                    variant="secondary"
+                  >
+                    <Plus className="w-5 h-5" />
+                    <p>Watchlist</p>
+                  </SquaredButton>
+                </React.Fragment>
+              )}
+
+              <SquaredButton variant="secondary">
+                <Heart className="w-5 h-5" />
+                <p>Favorite</p>
+              </SquaredButton>
+              <SquaredButton
+                onClick={() => {
+                  navigator.clipboard.writeText(window.location.href);
+                }}
+                variant="secondary"
+              >
+                <Share2 className="w-5 h-5" />
+                <p>Share</p>
+              </SquaredButton>
+            </div>
           </div>
-         </div>
-         <div className='xl:w-1/2 xl:block hidden'>
-         {trailerKey && (
-          <iframe 
-          className='w-full aspect-video rounded-md'
-          src={`https://www.youtube.com/embed/${trailerKey}`}
-          allow='autoplay; encrypted-media'
-          allowFullScreen
-          />
-         )}
-         </div>
+
+          <div className="flex-1 xl:block hidden pl-14">
+            {trailerKey && (
+              <iframe
+                className="w-full aspect-video rounded-md border-2 border-zinc-900/30"
+                src={`https://www.youtube.com/embed/${trailerKey}`}
+                allow="autoplay; encrypted-media"
+                allowFullScreen
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
