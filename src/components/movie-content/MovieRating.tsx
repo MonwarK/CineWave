60e8d00725acc68761972811c09260351fa5d4fc
Monwar Khan <monwar259@gmail.com>
2025-06-25 05:10:52 +0100

    Implemented Review Page UI

--- src/components/movie-content/MovieRating.tsx
+++ src/components/movie-content/MovieRating.tsx
@@ -1,31 +1,7 @@
-import { Movie } from "@/types/Movie";
-import { Star } from "lucide-react";
-import React from "react";
-
-const renderStars = (rating: number) => {
-  const stars = [];
-  const fullStars = Math.floor(rating / 2);
-  const hasHalfStar = rating % 2 >= 1;
-
-  for (let i = 0; i < fullStars; i++) {
-    stars.push(
-      <Star key={i} className="w-5 h-5 fill-yellow-400 text-yellow-400" />
-    );
-  }
-
-  if (hasHalfStar) {
-    stars.push(
-      <Star key="half" className="w-5 h-5 fill-yellow-400/50 text-yellow-400" />
-    );
-  }
-
-  const remainingStars = 5 - Math.ceil(rating / 2);
-  for (let i = 0; i < remainingStars; i++) {
-    stars.push(<Star key={`empty-${i}`} className="w-5 h-5 text-gray-300" />);
-  }
-
-  return stars;
-};
+import { Movie } from '@/types/Movie';
+import { Star } from 'lucide-react';
+import React from 'react';
+import StarRating from '../other/StarRating';
 
 export default function MovieRating({ movie }: { movie: Movie }) {
   return (
@@ -38,7 +14,7 @@ export default function MovieRating({ movie }: { movie: Movie }) {
               <div>TheMovieDB ({movie.vote_average.toFixed(1)}/10)</div>
             </div>
             <div className="flex items-center">
-              {renderStars(movie.vote_average)}
+              <StarRating rating={movie.vote_average} />
             </div>
           </div>
         </div>
