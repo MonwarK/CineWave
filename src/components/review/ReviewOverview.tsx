d0a76e4696ea9330c44739b6425cbb9253be93b5
Monwar Khan <monwar259@gmail.com>
2025-06-25 14:57:47 +0100

    using to fixed review

--- src/components/review/ReviewOverview.tsx
+++ src/components/review/ReviewOverview.tsx
@@ -11,7 +11,9 @@ export default function ReviewOverview({ voteAverage, voteCount }: Props) {
     <div>
       <div className="border border-orange-500 rounded-lg px-5 py-10 bg-gradient-to-r from-orange-800/50 to-zinc-900/50 text-center space-y-5">
         <div>
-          <div className="text-4xl font-bold mb-2">{voteAverage}</div>
+          <div className="text-4xl font-bold mb-2">
+            {voteAverage.toFixed(1)}
+          </div>
           <div className="flex justify-center">
             <StarRating rating={voteAverage} />
           </div>
