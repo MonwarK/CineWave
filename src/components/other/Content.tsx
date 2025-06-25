321c03964a9ed7fd38b0169bf0f9b7bdd554a5c1
Monwar Khan <monwar259@gmail.com>
2025-06-25 05:29:15 +0100

    adjusted content styling

--- src/components/other/Content.tsx
+++ src/components/other/Content.tsx
@@ -6,6 +6,8 @@ interface Props {
 
 export default function Content({ children }: Props) {
   return (
-    <div className="pt-[7rem] container max-w-6xl mx-auto px-6">{children}</div>
+    <div className="pt-[7rem] container max-w-screen-xl mx-auto px-6">
+      {children}
+    </div>
   );
 }
