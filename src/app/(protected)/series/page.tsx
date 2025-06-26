5b30f9b1ec07604618eb3fc252fcc32821e4d674
Monwar Khan <monwar259@gmail.com>
2025-06-26 15:08:05 +0100

    Set up pagination for trending, movies, and series

--- src/app/(protected)/series/page.tsx
+++ src/app/(protected)/series/page.tsx
@@ -1,8 +1,5 @@
 import Header from '@/components/main/Header';
-import MovieLandscapeThumbnail from '@/components/movie-card/MovieLandscapeThumbnail';
-import Content from '@/components/other/Content';
-import { Movie } from '@/types/Movie';
-import { fetchTopRated } from '@/utils/api';
+import MoviesListPage from '@/components/MoviesListPage.tsx/MoviesListPage';
 
 export const metadata = {
   title: 'TV Shows | Cinewave',
@@ -11,34 +8,15 @@ export const metadata = {
 };
 
 export default async function SeriesPage() {
-  const series = await fetchTopRated('tv');
-
   return (
     <div>
       <Header />
 
-      <Content>
-        <div className="flex flex-col justify-between space-y-10">
-          <div className="space-y-5">
-            <h1 className="text-3xl font-semibold ">TV Shows</h1>
-            <p className="text-gray-300">
-              Explore thousands of TV shows across every genre, from gripping
-              crime dramas and lively talk shows to captivating anime and more.
-              Whatever you&apos;re into, we&apos;ve got something for you.
-            </p>
-          </div>
-
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {series.map((serie: Movie) => (
-              <MovieLandscapeThumbnail
-                key={`movie-thumbnail-${serie.id}`}
-                isMovie={false}
-                movie={serie}
-              />
-            ))}
-          </div>
-        </div>
-      </Content>
+      <MoviesListPage
+        title="TV Shows"
+        description="Explore thousands of TV shows across every genre, from gripping crime dramas and lively talk shows to captivating anime and more. Whatever you're into, we've got something for you."
+        isMovie={false}
+      />
     </div>
   );
 }
