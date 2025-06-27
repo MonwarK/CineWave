cff5a2225d3f0acfde082b5e06e05a87c79d13ce
Monwar Khan <monwar259@gmail.com>
2025-06-27 17:43:15 +0100

    Added profile page option to clerk modal + adjusted profile routes

---
+++ src/app/(protected)/profile/subscription/page.tsx
@@ -0,0 +1,23 @@
+import ProfilePage from '@/components/profile/ProfilePage';
+import { getCurrentPlan } from '@/utils/getCurrentPlan';
+import { auth } from '@clerk/nextjs/server';
+
+export const metadata = {
+  title: 'Profile | Cinewave',
+};
+
+export default async function CustomProfilePage() {
+  const { has } = await auth();
+
+  const hasStarterPlan = has({ plan: 'starter' });
+  const hasStandardPlan = has({ plan: 'standard_plan' });
+  const hasPremiumPlan = has({ plan: 'premium' });
+
+  const currentPlan = getCurrentPlan(
+    hasStarterPlan,
+    hasStandardPlan,
+    hasPremiumPlan
+  );
+
+  return <ProfilePage currentPlan={currentPlan} />;
+}
--- src/app/db/queries.ts
+++ src/app/db/queries.ts
@@ -1,28 +1,31 @@
-import { supabase } from "@/libs/supabaseClient";
+import { supabase } from '@/libs/supabaseClient';
 
 export async function getUserData(id: string) {
-  if(!id) return;
+  if (!id) return;
 
-  const {data, error} = await supabase.from("users").select().eq("id", id);
+  const { data, error } = await supabase.from('users').select().eq('id', id);
 
-  if(error) {
-    console.log("Error fetching user data from supabase", error);
+  if (error) {
+    console.log('Error fetching user data from supabase', error);
     return;
   }
 
-  return data[0]; 
+  return data[0];
 }
 
 export async function getUserReviews(id: string) {
-  if(!id) return;
+  if (!id) return;
 
-  const {data, error} = await supabase.from("movie_reviews").select().eq("user_id", id);
+  const { data, error } = await supabase
+    .from('movie_reviews')
+    .select()
+    .eq('user_id', id)
+    .order('updated_at', { ascending: false });
 
-  if(error) {
-    console.log("Error fetching user data from supabase", error);
+  if (error) {
+    console.log('Error fetching user data from supabase', error);
     return;
   }
 
-  return data; 
+  return data;
 }
-
