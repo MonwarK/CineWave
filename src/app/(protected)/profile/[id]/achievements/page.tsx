649ed271a373b9295646c3860406f4d4de1373dd
Monwar Khan <monwar259@gmail.com>
2025-07-01 21:12:09 +0100

    adjusted props

--- src/app/(protected)/profile/[id]/achievements/page.tsx
+++ src/app/(protected)/profile/[id]/achievements/page.tsx
@@ -5,7 +5,6 @@ import {
   getUserReviews,
 } from '@/app/db/queries';
 import UserProfile from '@/components/profile/UserProfile';
-import { supabase } from '@/libs/supabaseClient';
 
 import { Metadata, ResolvedMetadata } from 'next';
 
@@ -14,7 +13,7 @@ type Props = {
 };
 
 export async function generateMetadata(
-  { params }: { params: { id: string } },
+  { params }: Props,
   parent: ResolvedMetadata
 ): Promise<Metadata> {
   const { id } = params;
