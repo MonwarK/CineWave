c3f6a7b733fbfc9650dcb816cd03f9a9a52e06b6
Monwar Khan <monwar259@gmail.com>
2025-07-03 19:47:27 +0100

    implemented dropdown with achievement sections

--- src/components/profile/profile-achievements/UserAchievementSection.tsx
+++ src/components/profile/profile-achievements/UserAchievementSection.tsx
@@ -1,6 +1,8 @@
 import { Achievement } from '@/types/Achievements';
 import clsx from 'clsx';
-import React from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import React, { useState } from 'react';
 
 export default function UserAchievementSection({
   title,
@@ -11,37 +13,57 @@ export default function UserAchievementSection({
   achievements: Achievement[];
   isAchievementUnlocked: (id: string) => boolean;
 }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const Chevron = isOpen ? ChevronUp : ChevronDown;
+
   return (
     <div>
-      <div>
-        <h2 className="text-xl uppercase font-medium mb-5">{title}</h2>
+      <div className="flex items-center justify-between pb-5">
+        <h2 className="text-xl uppercase font-medium">{title}</h2>
+        <div>
+          <Chevron
+            className="cursor-pointer text-gray-500 hover:opacity-50 transition duration-100"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        </div>
       </div>
-      <div className="space-y-5">
-        {achievements?.map(achievement => (
-          <div
-            key={achievement.id}
-            className={clsx(
-              'bg-zinc-800 rounded-lg border border-zinc-700 p-5',
-              isAchievementUnlocked(achievement.id) &&
-                'opacity-30 grayscale-100'
-            )}
-          >
-            <div className="flex items-center gap-5">
-              <div>
-                <img
-                  className="w-16"
-                  src="https://img.icons8.com/?size=100&id=LnEviQCbWLnq&format=png&color=000000"
-                  alt={achievement.title}
-                />
+      <AnimatePresence>
+        <motion.div
+          className="overflow-hidden"
+          initial={{ height: 0 }}
+          animate={isOpen ? { height: 'auto' } : { height: 0 }}
+        >
+          <div className="grid md:grid-cols-2 gap-5">
+            {achievements?.map(achievement => (
+              <div
+                key={achievement.id}
+                className={clsx(
+                  'bg-zinc-800 rounded-lg border border-zinc-700 p-5',
+                  isAchievementUnlocked(achievement.id) &&
+                    'opacity-30 grayscale-100'
+                )}
+              >
+                <div className="flex items-center gap-5">
+                  <div>
+                    <img
+                      className="w-16"
+                      src="https://img.icons8.com/?size=100&id=LnEviQCbWLnq&format=png&color=000000"
+                      alt={achievement.title}
+                    />
+                  </div>
+                  <div>
+                    <h2 className="text-xl mb-1 font-bold">
+                      {achievement.title}
+                    </h2>
+                    <p className="text-gray-400">{achievement.description}</p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <h2 className="text-xl mb-1 font-bold">{achievement.title}</h2>
-                <p className="text-gray-400">{achievement.description}</p>
-              </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
