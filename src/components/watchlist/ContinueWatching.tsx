5b03230bc10f9824a39adc6e74284a4ce2c7256a
Monwar Khan <monwar259@gmail.com>
2025-07-03 20:03:39 +0100

    added map key

--- src/components/watchlist/ContinueWatching.tsx
+++ src/components/watchlist/ContinueWatching.tsx
@@ -70,10 +70,11 @@ export default function ContinueWatching({
                 drag="x"
                 dragConstraints={{ right: 0, left: dragWidth }}
                 className="flex gap-5 cursor-grab py-5"
-                viewport={{ once: true, amount: 0.3 }}
+                viewport={{ once: true, amount: 0.5 }}
               >
                 {continueWatching.map(media => (
                   <motion.div
+                    key={`continue-watching-${media.id}`}
                     variants={itemVariants}
                     className="flex-none w-3/4 md:w-1/2 lg:w-1/3"
                   >
